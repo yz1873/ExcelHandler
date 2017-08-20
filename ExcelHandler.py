ba6dda3e94293a8efdd9f733e45ed90f0972c95d
yz1873 <yz1873@126.com>
2017-08-20 21:11:12 +0800

    输出格式完成

--- ExcelHandler.py
+++ ExcelHandler.py
@@ -1,120 +1,76 @@
 import xlwt
-import math
-import pandas as pd
-import numpy as np
 import Data
 import Functions
-
-# 截至月（截止2017年7月各省分供应商落地份额使用率）
-currentMonth = 7
-
-#  路径前加r（原因：文件名中的 \U 开始的字符被编译器认为是八进制）
-#  保存输出数据的文档地址  Administrator
-resultFile_path = r"C:\Users\Administrator\Desktop\数据结果.xls"
-# resultFile_path = r"C:\Users\Zhang Yu\Desktop\数据结果.xls"
+import pymysql.cursors
 
 # 创建结果表
 wb = xlwt.Workbook(encoding='utf-8', style_compression=0)
 # 创建newsheet 第二参数用于确认同一个cell单元是否可以重设值。
 newsheet = wb.add_sheet('datasheet', cell_overwrite_ok=True)
 
+# 连接数据库统计数据
+connection = pymysql.connect(**Data.config)
+# 使用cursor()方法获取操作游标
+cursor = connection.cursor()
+# 执行sql语句
+try:
+    # 执行sql语句，进行查询
+    cursor.execute(Data.dldlSqlStr, Data.end_data)
+    dldlresult = cursor.fetchall()
+    Functions.countByRow(dldlresult, Data.dldlFrame, Data.dldlSupplier)
+    print(dldlresult)
+    print(Data.dldlFrame)
+    # cursor.execute(Data.kxSqlStr, Data.end_data)
+    # kxresult = cursor.fetchall()
+    # print(dldlresult)
+except:
+    print("Error: unable to fecth data")
+connection.close()
 
 #  Excel输出
 #  扩大1到100列的宽度
 for n in range(0, 100):
     newsheet.col(n).width = 256*11
 
-#  表头格式
-header_style = xlwt.easyxf('font: name 微软雅黑, height 220, bold on;')
-#  表行列名格式
-tablestyle = 'font: name 微软雅黑, height 180, bold on; '  #  粗体字
-tablestyle += 'align: horz centre, vert center, wrap on; '  #  居中,自动换行
-tablestyle += 'borders: left THIN, right THIN, top THIN, bottom THIN; '  #  边框
-table_style = xlwt.easyxf(tablestyle)
-#  正文格式
-textstyle = 'font: name 微软雅黑, height 180;'  #  粗体字
-textstyle += 'align: horz centre, vert center; '  #  居中
-tablestyle += 'borders: left THIN, right THIN, top THIN, bottom THIN; '  #  边框
-text_style = xlwt.easyxf(textstyle)
-text_style.num_format_str = "#,##0.00"
-# 百分比格式
-percent_font = xlwt.Font()
-percent_font.name = '微软雅黑'
-percent_font.height = 180
-percent_font.bold = True
-percent_borders = xlwt.Borders()
-percent_borders.left = xlwt.Borders.THIN
-percent_borders.right = xlwt.Borders.THIN
-percent_borders.top = xlwt.Borders.THIN
-percent_borders.bottom = xlwt.Borders.THIN
-percent_style = xlwt.XFStyle()
-percent_style.num_format_str = '0.00%'
-percent_style.font = percent_font
-percent_style.borders = percent_borders
-
 # 表头
 x = 0
 y = 0
-newsheet.write_merge(x, x, y+0, y+99, '截止2017年%d' % currentMonth + '月各省分供应商落地份额使用率', header_style)
+newsheet.write_merge(x, x, y+0, y+99, '截止%s' % Data.end_data + '各省分供应商落地份额使用率', Data.header_style)
 x += 1
-newsheet.write(x, y+0, '', table_style)
-newsheet.write(x, y+1, '', table_style)
-newsheet.write_merge(x, x, y+2, y+4, '中标情况基础数据', table_style)
-newsheet.write(x, y+5, '累计使用率', table_style)
-newsheet.write(x, y+6, '', table_style)
+newsheet.write(x, y+0, '', Data.table_style)
+newsheet.write(x, y+1, '', Data.table_style)
+newsheet.write_merge(x, x, y+2, y+4, '中标情况基础数据', Data.table_style)
+newsheet.write(x, y+5, '累计使用率', Data.table_style)
+newsheet.write(x, y+6, '', Data.table_style)
 y += 7
 for n in range(0, 31):
-    newsheet.write_merge(x, x, y + 3*n, y + 3*n + 2, Data.province[n], table_style)
+    newsheet.write_merge(x, x, y + 3*n, y + 3*n + 2, Data.province[n], Data.table_style)
 x += 1
 y = 0
 newsheet.col(x).height = 256*20
-newsheet.write(x, y+0, '设备类型', table_style)
-newsheet.write(x, y+1, '供应商', table_style)
-newsheet.write(x, y+2, '合计中标份额', table_style)
-newsheet.write(x, y+3, '中标份额占比', table_style)
-newsheet.write(x, y+4, '中标份额排名', table_style)
-newsheet.write(x, y+5, '本期主材数量', table_style)
-newsheet.write(x, y+6, '本期主材数量/C列合计中标份额*100%', table_style)
+newsheet.write(x, y+0, '设备类型', Data.table_style)
+newsheet.write(x, y+1, '供应商', Data.table_style)
+newsheet.write(x, y+2, '合计中标份额', Data.table_style)
+newsheet.write(x, y+3, '中标份额占比', Data.table_style)
+newsheet.write(x, y+4, '中标份额排名', Data.table_style)
+newsheet.write(x, y+5, '本期主材数量', Data.table_style)
+newsheet.write(x, y+6, '本期主材数量/C列合计中标份额*100%', Data.table_style)
 y += 7
 for n in range(0, 31):
-    newsheet.write(x, y+3*n, '其中：中标份额', table_style)
-    newsheet.write(x, y+3*n+1, '本期主材数量', table_style)
-    newsheet.write(x, y+3*n+2, '分省完成率=本省本期主材数量/本省中标份额*100%', table_style)
-
-
-
+    newsheet.write(x, y+3*n, '其中：中标份额', Data.table_style)
+    newsheet.write(x, y+3*n+1, '本期主材数量', Data.table_style)
+    newsheet.write(x, y+3*n+2, '分省完成率=本省本期主材数量/本省中标份额*100%', Data.table_style)
 x += 1
 y = 0
-newsheet.write_merge(x, x + len(Data.dldlSupplier), y, y, Data.dldlInfo, table_style)
-for n in range(0, len(Data.dldlSupplier)):
-    newsheet.write(x + n, y + 1, Data.dldlSupplier[n], table_style)
-    newsheet.write(x + n, y + 2, Data.dldlTotalBiddingData[n], table_style)
-    newsheet.write(x + n, y + 3, xlwt.Formula('C' + str(x + n + 1) + '/C' + str(x + len(Data.dldlSupplier) + 1)),
-                   percent_style)
-    newsheet.write(x + n, y + 4, n+1, table_style)
-    newsheet.write(x + n, y + 5, xlwt.Formula(Functions.mergeStr(x + n + 1)), table_style)
-    newsheet.write(x + n, y + 6, xlwt.Formula('F' + str(x + n + 1) + '/C' + str(x + len(Data.dldlSupplier) + 1)),
-                   percent_style)
-    for m in range(0, len(Data.province)):
-        newsheet.write(x + n, y + 7 + 3 * m, (Functions.intTran(Data.dldlBiddingFrame.iloc[n, m])), table_style)
-        newsheet.write(x + n, y + 8 + 3 * m, (Functions.intTran(Data.dldlFrame.iloc[n, m])), table_style)
-        newsheet.write(x + n, y + 9 + 3 * m, (0 if (Functions.intTran(Data.dldlBiddingFrame.iloc[n, m]) == 0)
-                                              else (Functions.intTran(Data.dldlFrame.iloc[n, m])/
-                                                    Functions.intTran(Data.dldlBiddingFrame.iloc[n, m]))), percent_style)
-x += len(Data.dldlSupplier)
-newsheet.write(x, y + 1, '%d家合计' % len(Data.dldlSupplier), table_style)
-newsheet.write(x, y + 2, Functions.totalCount(Data.dldlTotalBiddingData), table_style)
-newsheet.write(x, y + 3, 1, percent_style)
-newsheet.write(x, y + 4, '---', table_style)
-newsheet.write(x, y + 5, xlwt.Formula(Functions.mergeStr(x + 1)), table_style)
-newsheet.write(x, y + 6, xlwt.Formula('F' + str(x + 1) + '/C' + str(x + 1)), percent_style)
-for m in range(0, len(Data.province)):
-    newsheet.write(x, y + 7 + 3 * m, Functions.totalCountByProvince(m, Data.dldlBiddingFrame), table_style)
-    newsheet.write(x, y + 8 + 3 * m, Functions.totalCountByProvince(m, Data.dldlFrame), table_style)
-    newsheet.write(x, y + 9 + 3 * m, (0 if (Functions.totalCountByProvince(m, Data.dldlBiddingFrame) == 0)
-                                      else (Functions.totalCountByProvince(m, Data.dldlFrame)/Functions.
-                                            totalCountByProvince(m, Data.dldlBiddingFrame))), percent_style)
+
+Functions.writeByGoods(x, y, Data.dldlSupplier, Data.dldlInfo, Data.dldlTotalBiddingData, Data.dldlBiddingFrame,
+                       Data.dldlFrame, newsheet, Data.table_style, Data.text_style, Data.percent_style)
+x += (len(Data.dldlSupplier)+1)
+
+Functions.writeByGoods(x, y, Data.kxSupplier, Data.kxInfo, Data.kxTotalBiddingData, Data.kxBiddingFrame,
+                       Data.kxFrame, newsheet, Data.table_style, Data.text_style, Data.percent_style)
+x += (len(Data.kxSupplier)+1)
+
+wb.save(Data.resultFile_path)
 
 
-
-wb.save(resultFile_path)
--- Functions.py
+++ Functions.py
@@ -1,15 +1,12 @@
 import math
 import Data
+import xlwt
 
 def totalCount(list):
     result = 0
     for n in range(0, len(list)):
         result += list[n]
     return result
-
-def percentTran(int):
-    return format(int, '.0%')
-    # return str(round((int*100), 2))+'%'
 
 def intTran(dataframeloc):
     return 0 if math.isnan(dataframeloc) else int(dataframeloc)
@@ -22,3 +19,39 @@ def mergeStr(n):
     return 'I'+str(n)+'+L'+str(n)+'+O'+str(n)+'+R'+str(n)+'+U'+str(n)+'+X'+str(n)+'+AA'+str(n)+'+AD'+str(n)+'+AG'+str(n)+'+AJ'+str(n)+'+AM'\
            +str(n)+'+AP'+str(n)+'+AS'+str(n)+'+AV'+str(n)+'+AY'+str(n)+'+BB'+str(n)+'+BE'+str(n)+'+BH'+str(n)+'+BK'+str(n)+'+BN'+str(n)+'+BQ'\
            +str(n)+'+BT'+str(n)+'+BW'+str(n)+'+BZ'+str(n)+'+CC'+str(n)+'+CF'+str(n)+'+CI'+str(n)+'+CL'+str(n)+'+CO'+str(n)+'+CR'+str(n)+'+CU'+str(n)
+
+def writeByGoods(x, y, GoodsSupplier, GoodsInfo, GoodsTotalBiddingData, GoodsBiddingFrame, GoodsFrame, currentsheet,
+                 table_style, text_style, percent_style):
+    currentsheet.write_merge(x, x + len(GoodsSupplier), y, y, GoodsInfo, table_style)
+    for n in range(0, len(GoodsSupplier)):
+        currentsheet.write(x + n, y + 1, GoodsSupplier[n], table_style)
+        currentsheet.write(x + n, y + 2, GoodsTotalBiddingData[n], text_style)
+        currentsheet.write(x + n, y + 3, xlwt.Formula('C' + str(x + n + 1) + '/C' + str(x + len(GoodsSupplier) + 1)),
+                       percent_style)
+        currentsheet.write(x + n, y + 4, n+1, text_style)
+        currentsheet.write(x + n, y + 5, xlwt.Formula(mergeStr(x + n + 1)), text_style)
+        currentsheet.write(x + n, y + 6, xlwt.Formula('F' + str(x + n + 1) + '/C' + str(x + len(GoodsSupplier) + 1)),
+                       percent_style)
+        for m in range(0, len(Data.province)):
+            currentsheet.write(x + n, y + 7 + 3 * m, (intTran(GoodsBiddingFrame.iloc[n, m])), text_style)
+            currentsheet.write(x + n, y + 8 + 3 * m, (intTran(GoodsFrame.iloc[n, m])), text_style)
+            currentsheet.write(x + n, y + 9 + 3 * m, (0 if (intTran(GoodsBiddingFrame.iloc[n, m]) == 0)
+                                                  else (intTran(GoodsFrame.iloc[n, m])/
+                                                        intTran(GoodsBiddingFrame.iloc[n, m]))), percent_style)
+    x += len(GoodsSupplier)
+    currentsheet.write(x, y + 1, '%d家合计' % len(GoodsSupplier), table_style)
+    currentsheet.write(x, y + 2, totalCount(GoodsTotalBiddingData), text_style)
+    currentsheet.write(x, y + 3, 1, percent_style)
+    currentsheet.write(x, y + 4, '---', table_style)
+    currentsheet.write(x, y + 5, xlwt.Formula(mergeStr(x + 1)), text_style)
+    currentsheet.write(x, y + 6, xlwt.Formula('F' + str(x + 1) + '/C' + str(x + 1)), percent_style)
+    for m in range(0, len(Data.province)):
+        currentsheet.write(x, y + 7 + 3 * m, totalCountByProvince(m, GoodsBiddingFrame), text_style)
+        currentsheet.write(x, y + 8 + 3 * m, totalCountByProvince(m, GoodsFrame), text_style)
+        currentsheet.write(x, y + 9 + 3 * m, (0 if (totalCountByProvince(m, GoodsBiddingFrame) == 0)
+                                          else (totalCountByProvince(m, GoodsFrame)/
+                                                totalCountByProvince(m, GoodsBiddingFrame))), percent_style)
+
+def countByRow(result, dataframe, supplier):    # TODO 永不用判断是NaN
+    for row in result:
+        dataframe.iloc[supplier.index(row[2]), Data.province.index(row[0])] += row[1]
